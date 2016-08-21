0c94d63f680606ffaf4fcc0554d56385a07a66e6
Ryan Long <ryanlong.long@gmail.com>
2016-08-21 06:40:23 -0400

    as far as I've gotten -_-

--- scripts/file1.js
+++ scripts/file1.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
-  var i = $('#container div').length
+  //var i = $('#container div').length
   for (var i = 1; i <=16; i++) {
+    $('#container').append("<div class='row' id='row"+ i +"\
+      'style='height:10px; margin: 0px'></div>");
+    for (x = 1; x <=16; x++) {
+      $('#row'+i).append("<div style='padding-top: 0px margin-top:0px;\
+                        display:inline-block; background-color:white;\
+                        width:10px; height:10px;\
+                        border: 1px solid black;'></div>")
 
-  for (x = 1; x <=16; x++) {
-  $('#container').append("<div style='margin: auto;\
-    display:inline-block; background-color:white;\
-    width:50px; height:50px;\
-    border: 1px solid black;'></div>")
-  console.log("Row "+i+" complete!"); 
-    } 
+        console.log(x)
+    }
+    console.log("Row "+i+" complete!");
   }
 })
