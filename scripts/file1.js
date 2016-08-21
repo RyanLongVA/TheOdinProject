35ddd6993e765cad362ae0b3f814e3d0e79b2f45
Ryan Long <ryanlong.long@gmail.com>
2016-08-21 00:32:06 -0400

    #2

--- scripts/file1.js
+++ scripts/file1.js
@@ -1,8 +1,13 @@
 $(document).ready(function() {
   var i = $('#container div').length
-  for (i = 0; i <= 16; i++) {
-  $('#container').append("<div class='row1'></div>")
-  console.log($('#container div').length)
+  for (var i = 1; i <=16; i++) {
+
+  for (x = 1; x <=16; x++) {
+  $('#container').append("<div style='padding-top: 0px margin-top:0px;\
+    display:inline-block; background-color:white;\
+    width:50px; height:50px;\
+    border: 1px solid black;'></div>")
+  console.log("Row "+i+" complete!"); 
+    } 
   }
-  console.log("complete");
 })
