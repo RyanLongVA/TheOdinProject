a5b45a2d4a515c49acad1feda65b27a8343ffa74
Ryan Long <ryanlong.long@gmail.com>
2016-08-21 00:54:24 -0400

    Box fix

--- scripts/file1.js
+++ scripts/file1.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   for (var i = 1; i <=16; i++) {
 
   for (x = 1; x <=16; x++) {
-  $('#container').append("<div style='padding-top: 0px margin-top:0px;\
+  $('#container').append("<div style='margin: auto;\
     display:inline-block; background-color:white;\
     width:50px; height:50px;\
     border: 1px solid black;'></div>")
