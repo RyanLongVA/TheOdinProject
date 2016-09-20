7223bb6c4a0006d142eab253b9e0fb420a64b14b
Ryan Long <ryanlong.long@gmail.com>
2016-09-20 06:44:19 -0400

    Beginning 05 with given up 06

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,48 +1,10 @@
-#write your code here
-def translate input
-    vowels = ["a", "e", "i", "o"]
-    output = input.split("")
-    if (output.include?(" ") == false)
-    i = 0
-    #Method for inputs with no spaces
-    while (i == 0) 
-      #First Letter is not a vowel
-      if ((vowels.include?(output[i])) == false)
-          output.insert((output.length - 1), output.delete_at(i))
-          
-      else
-         #Is a vowel
-          output = output.join + "ay"
-          i += 1
-          break
+def translate input 
+  input.split.map do |word|
+    if %(a e i o).include? word[0]
+      "#{word}ay"
+    else 
+      word.sub(/([^aeio]+)(.*)/, 
+        "\\2\\1ay")
       end
-    end
-  
-      
-  #Method for inputs with spaces
-  else
-    i = output.index(output.first)
-    
-      output = output.join.split
-      while (i <(output.length - 1))
-      output[i] = output[i].split("")
-      x = output[i].first
-        while ((output[i].index(output[i].first))<= (output[i].length - 1))
-        
-          if ((vowels.include?(output[i][x])) == false)
-            output[i].insert((output[i].length - 1), output[i].delete_at(x))
-            x += 1 
-          else
-            output[i] = output[i].join + "ay"
-            i += 1
-            break
-          end
-        end
-      end
-    output = output.join(" ")
-    end 
-    return output
-end 
-          
-        ######### I rewrote this like twice and changed SO much. 
-        ######### Skip Authorized ;)
+    end.join(" ")
+  end 
--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,16 @@
 class Book
 # write your code here
+  @title = String.new
+  attr_accessor :title
+  no_cap = ["the", "a", "an", "a", "and", "in", "of"]
+  def title
+  # book.title.split.map do |word|
+  #   if (book.title[0] == word)
+  #     #Do Nothing
+  #   elsif no_cap.include?(word)  
+  #   else word = word.capitalize
+  #   end 
+  # end
+  # return book.title.join 
+end
 end
